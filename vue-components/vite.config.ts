7d94c33a0ac4bf84581850e73742d3701812a887
Hannes Zbinden <hanneszbinden@outlook.com>
2022-03-13 15:30:02 +0100

    remove default-content, add windicss.

--- vue-components/vite.config.ts
+++ vue-components/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Icons from "unplugin-icons/vite";
+import WindiCSS from "vite-plugin-windicss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Icons({ compiler: "vue3" })],
+  plugins: [vue(), Icons({ compiler: "vue3" }), WindiCSS()],
 });
